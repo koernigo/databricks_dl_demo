d25fac0b88b2168d2d8e9b50185210e97228d912
ashleytrainor <ashleytrainor>
2021-06-03 01:36:58 +0000

    update udf to pandas udf

--- Deep Learning Image Prep - Initial Data Load.py
+++ Deep Learning Image Prep - Initial Data Load.py
@@ -2,7 +2,7 @@
 # MAGIC %md
 # MAGIC ## Data Preparation - Initial Data Load
 # MAGIC 
-# MAGIC This loads the Caltech 256 images from .jpg files, resizes them to 299x299, and extracts the label from the file name. The result is written to Parquet and Delta tables. This only needs to be run once.
+# MAGIC This loads the Caltech 256 images from .jpg files and extracts the label from the file name. The result is written a Delta table. This only needs to be run once. Manually labelled images can later be added to this table for increased training set. 
 
 # COMMAND ----------
 
@@ -19,16 +19,25 @@ table_path=dbutils.widgets.get("table_path")
 
 # COMMAND ----------
 
+a = [1,2,3,4,5]
+a[-2]
+
+# COMMAND ----------
+
 import io
 import numpy as np
 from PIL import Image
 from pyspark.sql.types import BinaryType, IntegerType
+from pyspark.sql.functions import col, pandas_udf
 
 def file_to_label(path):
   # .../043.coin/043_0042.jpg -> 043.coin -> 043 -> 43
-  return int(path.split("/")[-2].split(".")[-2])
+  
+  return path.str.split(pat='/', expand=True).iloc[:,-2] \
+             .str.split(pat=".", expand=True).iloc[:,-2] \
+             .astype(int)
 
-file_to_label_udf = udf(file_to_label, IntegerType())
+file_to_label_udf = pandas_udf(file_to_label, IntegerType())
 
 # COMMAND ----------
 
@@ -39,7 +48,11 @@ from pyspark.sql.types import BinaryType, IntegerType
 
 # COMMAND ----------
 
-raw_image_df = spark.read.format("binaryFile").option("pathGlobFilter", "*.jpg").option("recursiveFileLookup", "true").load(caltech_256_path).repartition(64).limit(100)
+raw_image_df = spark.read.format("binaryFile") \
+                  .option("pathGlobFilter", "*.jpg") \
+                  .option("recursiveFileLookup", "true") \
+                   .load(caltech_256_path).limit(100)
+
 image_df = raw_image_df.withColumn("label",file_to_label_udf("path")).cache()
 
 # COMMAND ----------
